c7d4011a0087a739b5d44faaf9d571c32e490cfe
Arpit Trivedi <arpittrivedi2425@gmail.com>
2020-03-09 00:34:02 +0530

    Create and read functionality

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -4,25 +4,37 @@ const tours = JSON.parse(
   fs.readFileSync(`${__dirname}/../dev-data/data/tours-simple.json`, 'utf-8')
 );
 
-exports.getAllTours = (req, res) => {
-  res.status(200).json({
-    status: 'success',
-    result: tours.length,
-    data: {
-      requestedAt: req.requestedAt,
-      tours
-    }
-  });
+exports.getAllTours = async (req, res) => {
+  try {
+    const tours = await tourModel.find();
+    res.status(201).json({
+      status: 'Success',
+      result: tours.length,
+      data: {
+        tours
+      }
+    });
+  } catch (err) {
+    res.status(500).json({
+      status: 'Fail'
+    });
+  }
 };
-exports.getTour = (req, res) => {
-  const id = req.params.id * 1;
-  const tour = tours.find(el => el.id === id);
-  res.status(200).json({
-    status: 'success',
-    data: {
-      tour
-    }
-  });
+exports.getTour = async (req, res) => {
+  try {
+    const tour = await tourModel.findById(req.params.id);
+    res.status(201).json({
+      status: 'Success',
+
+      data: {
+        tour
+      }
+    });
+  } catch (err) {
+    res.status(500).json({
+      status: 'Fail'
+    });
+  }
 };
 exports.updateTour = (req, res) => {
   res.status(200).json({
@@ -34,17 +46,18 @@ exports.deleteTour = (req, res) => {
     status: 'success'
   });
 };
-exports.createTour = (req, res) => {
-  const newId = tours[tours.length - 1].id + 1;
-
-  const newTour = Object.assign({ id: newId }, req.body);
-  tours.push(newTour);
-  fs.writeFile(
-    `${__dirname}/dev-data/data/tours-simple.json`,
-    JSON.stringify(tours),
-    err => {
-      console.log(err);
-    }
-  );
-  res.send('Ok');
+exports.createTour = async (req, res) => {
+  try {
+    const newtour = await tourModel.create(req.body);
+    res.status(201).json({
+      status: 'Success',
+      data: {
+        newtour
+      }
+    });
+  } catch (err) {
+    res.status(500).json({
+      status: 'Fail'
+    });
+  }
 };
