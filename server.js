c6eabb08e145eb6c803fa5a659ad14e318d0ea93
Arpit Trivedi <arpittrivedi2425@gmail.com>
2020-03-08 23:16:50 +0530

    some upate

--- server.js
+++ server.js
@@ -5,8 +5,19 @@ const db = process.env.DATABASE.replace(
   '<PASSWORD>',
   process.env.DATABASE_PASSWORD
 );
-mongoose.connect(db).then(() => console.log('Database connected'));
 
+mongoose
+  .connect(db, {
+    useUnifiedTopology: true,
+    useNewUrlParser: true
+  })
+  .then(() => console.log('Database connected'))
+  .catch(e => {});
+var dbs = mongoose.connection;
+// dbs.once('error', () => {
+//   console.log('Message from connections');
+// });
+// dbs.on('error', console.error.bind(console, 'connection error:'));
 const app = require('./app');
 app.listen(3000, () => {
   console.log('Connecting to server');
