a880d7c088261fb09a4d5b88830d2f61eb868987
Arpit Trivedi <arpittrivedi2425@gmail.com>
2020-03-15 00:39:52 +0530

    Data Modeling

--- app.js
+++ app.js
@@ -1,12 +1,38 @@
 const express = require('express');
+const rateLimit = require('express-rate-limit');
 const tourRoutes = require('./routes/tourRoutes');
 const cors = require('cors');
+const helmet = require('helmet');
+const xss = require('xss-clean');
+const hpp = require('hpp');
+const mongoSanitize = require('express-mongo-sanitize');
 const userRoutes = require('./routes/userRoutes');
 const globalErrorHandler = require('./controllers/errorController');
 const appError = require('./utils/appError');
 const app = express();
+app.use(helmet());
+const limiter = rateLimit({
+  max: 100,
+  windowMs: 60 * 60 * 1000,
+  message: 'Too many request please try after an hour'
+});
+app.use('/api', limiter);
 app.use(express.static(`${__dirname}/public`));
 app.use(cors());
+app.use(
+  hpp({
+    whitelist: [
+      'duration',
+      'ratingsQuantity',
+      'ratingsAverage',
+      'maxGroupSize',
+      'difficulty',
+      'price'
+    ]
+  })
+);
+app.use(xss());
+app.use(mongoSanitize());
 app.use(express.json());
 app.use((req, res, next) => {
   req.requestedAt = new Date().toLocaleString();
