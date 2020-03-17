14d8d1de760f1616360e39bc5604e392dda78e91
Arpit Trivedi <arpittrivedi2425@gmail.com>
2020-03-17 16:56:38 +0530

    overview

--- controllers/viewController.js
+++ controllers/viewController.js
@@ -1,8 +1,12 @@
-exports.overview = (req, res) => {
+const Tour = require('../models/tourModel');
+const catchAsync = require('../utils/catchAsync');
+exports.overview = catchAsync(async (req, res) => {
+  const tours = await Tour.find();
   res.status(200).render('overview', {
-    title: 'All Tours'
+    title: 'All Tours',
+    tours
   });
-};
+});
 exports.tour = (req, res) => {
   res.status(200).render('tour', {
     title: 'Tour'
--- routes/viewRoutes.js
+++ routes/viewRoutes.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express.Router();
 const viewController = require('./../controllers/viewController');
-app.get('/overview', viewController.overview);
+app.get('/', viewController.overview);
 app.get('/tour', viewController.tour);
 module.exports = app;
