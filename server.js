2f87bdcedbb2473e9710b5f4578cba712bf022e0
Arpit Trivedi <31933155+arpit2425@users.noreply.github.com>
2020-05-16 16:30:04 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,12 +1,7 @@
 const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 const app = require('./app');
-process.on('uncaughtException', err => {
-  console.log('UnCaught Exception Shutting Down Server');
-  server.close(() => {
-    process.exit(1);
-  });
-});
+
 dotenv.config({ path: `${__dirname}/config.env` });
 const db = process.env.DATABASE.replace(
   '<PASSWORD>',
@@ -20,9 +15,15 @@ mongoose
   })
   .then(() => console.log('Database connected'))
   .catch(e => {});
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 5000;
 const server = app.listen(port, () => {
   console.log('Connecting to server');
+});
+process.on('uncaughtException', err => {
+  console.log('UnCaught Exception Shutting Down Server');
+  server.close(() => {
+    process.exit(1);
+  });
 });
 process.on('unhandledRejection', err => {
   console.log('Unhandled Rejection Shutting Down Server', err);
